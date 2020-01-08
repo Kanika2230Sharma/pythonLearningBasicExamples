08d0416af79582fde3ef5663639bfe2b569c39aa
Kanika2230Sharma <57859962+Kanika2230Sharma@users.noreply.github.com>
2020-01-09 00:03:20 +0530

    Delete Password exercise.py

--- python Basics/Password exercise.py
+++
@@ -1,4 +0,0 @@
-userName = input('Enter username')
-passWord  = input('Enter password')
-codedPass = '*' * passWord.len()
-print(f'Hi {userName}, your password {codedPass} is {passWord.len()} long' )
